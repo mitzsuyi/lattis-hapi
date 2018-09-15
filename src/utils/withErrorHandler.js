efb1b5e0f5bd026c1d92e9643b02b60c17113d17
mitzsuyi <mitzsuyi@users.noreply.github.com>
2018-09-15 21:06:07 +0800

    Update withErrorHandler.js

--- src/utils/withErrorHandler.js
+++ src/utils/withErrorHandler.js
@@ -6,8 +6,7 @@ const withErrorHandler = async (request, reply, handler)=>{
   try {
     const result = await handler(request, reply)
     return result
-  } catch(err){
-    console.log(err)
+  } catch(err) {
     if(err.isBoom) return err
     return boom.boomify(err)
   }
