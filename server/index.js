7ac01476df344b1bb8aac47798589c8b8955bd3b
mitzsuyi <mitzsuyi@users.noreply.github.com>
2018-09-15 18:42:25 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ const server = new Hapi.Server({
       failAction: async (request, h, err) => {
         if (process.env.NODE_ENV === 'production') {
           // In prod, log a limited error message and throw the default Bad Request error.
-          console.error('ValidationError:', err.message); // Better to use an actual logger here.
-          console.log(request.payload,request.params)
+          console.error('ValidationError:', err.message); // Better to use an actual logger here.         
           throw Boom.badRequest(`Invalid request payload input`);
         } else {
           // During development, log and respond with the full error.
