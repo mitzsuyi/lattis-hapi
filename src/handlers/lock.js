b91c81bbb94f413da72d3187516b7746dcaeaff4
mitzsuyi <mitzsuyi@users.noreply.github.com>
2018-09-15 19:37:24 +0800

    Update lock.js

--- src/handlers/lock.js
+++ src/handlers/lock.js
@@ -8,7 +8,6 @@ const noContent = (reply)=> reply.response().code(204)
 const getLockWithAuth = async(request, cond)=>{
    const userId = request.auth.credentials.id
    const lock = await Lock.where(cond).fetch()
-   console.log('got lock',lock)
    if (!lock){
     throw Boom.notFound("Lock does not exist")
    } else {
